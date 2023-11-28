860034083fd6f7cdb6c5f44bd7479d1a6f21c07d
Snails <renish88@gmail.com>
2023-11-28 16:31:27 +0900

    추가) 방 타입, 고정 방, 반고정 방 기능
    방 타입은 입력한 확률에 따라 정해짐
    
    고정 방 기능
    원하는 층전체를 원하는 방 타입으로 고정
    
    반(Anti)고정 기능
    원하는 층에 해당 방 타입은 등장하지 않게함

--- Scripts/map_manager.gd
+++ Scripts/map_manager.gd
@@ -5,6 +5,7 @@ const grid = preload("res://Scenes/grid.tscn")
 
 var RANDOM_PERCENT = 65
 
+@export_group("Map Generation")
 @export var mapSize = Vector2(1,1)
 @export var gridCount = Vector2(1,1)
 @export var horizontal = false
@@ -12,6 +13,27 @@ var RANDOM_PERCENT = 65
 @export var pathLoopCount = 2
 @export var maxSideRange = 1
 @export var shakePosition = 0.01
+
+# 종류, 확률
+@export_subgroup("Room Type")
+@export var type: Array[String]
+@export var probability: Array[int]
+
+# 고정할 층, 종류
+@export_subgroup("Fixed Room")
+@export var floor: Array[int]
+@export var roomType: Array[String]
+
+@export_subgroup("Anti Fixed Room")
+@export var ignoreFloor: Array[int]
+@export var ignoreType: Array[String]
+
+# 방 이미지
+@export_subgroup("Room Texture")
+@export var normal: Array[Texture2D]
+@export var pressed: Array[Texture2D]
+@export var hover: Array[Texture2D]
+@export var disabled: Array[Texture2D]
 
 @onready var map = $Map
 
@@ -29,20 +51,20 @@ func _ready():
 	remove_empty_room()
 	set_selectable(true)
 	
-func select_room(value):
+func select_room(_value):
 	set_selectable(false)
-	currentRoom = value
+	currentRoom = _value
 	# 임시로 바로 선택가능하게 해둠
 	set_selectable(true)
 	
-func set_selectable(value):
+func set_selectable(_value):
 	if currentRoom != null:
 		for i in range(currentRoom.nextRoom.size()):
-			currentRoom.nextRoom[i].set_selectable(value)
+			currentRoom.nextRoom[i].set_selectable(_value)
 	else:
 		for i in range(breadth):
 			if exist_room(0, i):
-				get_room(0, i).set_selectable(value)
+				get_room(0, i).set_selectable(_value)
 		
 func make_grid():
 	if horizontal:
@@ -62,6 +84,7 @@ func make_grid():
 			map.add_child(grids[i][j])
 			var roomInstance = room.instantiate()
 			roomInstance.room_selected.connect(select_room)
+			select_room_type(i, roomInstance)
 			grids[i][j].add_child(roomInstance)
 			grids[i][j].size = Vector2(map.size.x / gridCount.x, map.size.y / gridCount.y)
 			roomInstance.position = Vector2(grids[i][j].size.x * 0.5, grids[i][j].size.y * 0.5)
@@ -91,8 +114,45 @@ func remove_empty_room():
 				get_room(i, j).queue_free()
 		
 
-func get_room(x, y):
-	return grids[x][y].get_child(0)
+func select_room_type(_depth, _instance):
+	for i in range(floor.size()):
+		if floor[i] == _depth + 1:
+			var texIndex = 0
+			for j in range(type.size()):
+				if type[j] == roomType[i]:
+					texIndex = j
+					break
+			_instance.set_type(roomType[i], normal[texIndex], pressed[texIndex], hover[texIndex], disabled[texIndex])
+			return
+	
+	var totalP = 0
+	for i in range(probability.size()):
+		totalP += probability[i]
+	
+	for i in range(ignoreFloor.size()):
+		if _depth + 1 == ignoreFloor[i]:
+			for j in range(type.size()):
+				if ignoreFloor[i] == _depth + 1 and ignoreType[i] == type[j]:
+					totalP -= probability[j]
+					break
+	
+	var rand = randi_range(0, totalP)
+	for i in range(probability.size()):
+		var ignore = false
+		for j in range(ignoreFloor.size()):
+			if _depth + 1 == ignoreFloor[j]:
+				if ignoreFloor[j] == _depth + 1 and ignoreType[j] == type[i]:
+					ignore = true
+		if ignore:
+			continue
+		if rand <= probability[i]:
+			_instance.set_type(type[i], normal[i], pressed[i], hover[i], disabled[i])
+			return
+		else:
+			rand -= probability[i]
 
-func exist_room(x, y):
-	return grids[x][y].get_child_count() > 0
+func get_room(_x, _y):
+	return grids[_x][_y].get_child(0)
+
+func exist_room(_x, _y):
+	return grids[_x][_y].get_child_count() > 0
--- Scripts/room.gd
+++ Scripts/room.gd
@@ -1,7 +1,8 @@
 extends Control
 
-signal room_selected(value)
+signal room_selected(_value)
 
+var type = ""
 var nextRoom = []
 var connected = false
 
@@ -14,19 +15,27 @@ func _draw():
 	for i in range(nextRoom.size()):
 		draw_line(Vector2.ZERO, nextRoom[i].global_position - self.global_position, Color.WHITE, 1)
 
-func add_path(path):
-	if nextRoom.find(path) < 0:
-		nextRoom.append(path)
-		path.connected = true
+func add_path(_path):
+	if nextRoom.find(_path) < 0:
+		nextRoom.append(_path)
+		_path.connected = true
 
 func get_path_count():
 	return nextRoom.size()
 
-func set_selectable(value):
-	if value:
+func set_selectable(_value):
+	if _value:
 		button.disabled = false
 	else:
 		button.disabled = true
+		
+func set_type(_value, _normal, _pressed, _hover, _disabled):
+	type = _value
+	get_child(0).texture_normal = _normal
+	get_child(0).texture_pressed = _pressed
+	get_child(0).texture_hover = _hover
+	get_child(0).texture_disabled = _disabled
+	
 
 func _on_button_pressed():
 	emit_signal("room_selected", self)
